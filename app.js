0ed61bd48487a9b209d19b2c67e85fc5a30578ad
Joe Mac <jhmccarney@gmail.com>
2024-02-22 16:38:48 +0000

    completed 9

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const {
   getCommentsById,
   postComment,
   patchArticles,
+  deleteComments,
 } = require("./controller/api-controllers");
 const app = express();
 
@@ -26,24 +27,28 @@ app.post("/api/articles/:article_id/comments", postComment);
 
 app.patch("/api/articles/:article_id", patchArticles);
 
-app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  } else next(err);
-});
+app.delete("/api/comments/:comment_id", deleteComments);
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "Bad request" });
-  } else if (err.status && err.msg) {
-    res.status(err.status).send(err.msg);
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Not found" });
   } else {
-    res.status(404).send({ msg: "Not Found" });
+    next(err);
   }
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
   res.status(500).send({ msg: "Internal Server Error" });
 });
+
 module.exports = app;
