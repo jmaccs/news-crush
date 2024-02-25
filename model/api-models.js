16231c149c0c043707fb1884d6fa5f80f7ea0bac
Joe Mac <jhmccarney@gmail.com>
2024-02-25 10:51:15 +0000

    completed 11

--- model/api-models.js
+++ model/api-models.js
@@ -14,54 +14,57 @@ exports.selectArticle = (article_id) => {
   `);
   return db.query(sql, [article_id]).then(({ rows }) => {
     const article = rows[0];
+
     if (!article) {
       return Promise.reject({ status: 404, msg: "Not found" });
     }
     return article;
   });
 };
+exports.getAllArticles = (topic) => {
+  let valueArr = [];
+  let query = `
+    SELECT articles.*,
+           COUNT(comments.article_id)::int AS comment_count
+    FROM articles  
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+  `;
+  let groupAndOrder = `
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC
+  `;
 
-exports.getAllArticles = () => {
-  let sql = `
-  SELECT
-  articles.author,
-  articles.title,
-  articles.article_id,
-  articles.topic,
-  articles.created_at,
-  articles.votes,
-  articles.article_img_url,
-  COUNT(comments.article_id) AS comment_count
-FROM
-  articles 
-LEFT JOIN
-  comments ON articles.article_id = comments.article_id
-GROUP BY
-  articles.article_id
-ORDER BY
-  articles.created_at DESC;`;
-  return db.query(sql).then((data) => {
-    return data.rows;
-  });
+  if (topic) {
+    return checkExists("topics", "slug", topic).then(() => {
+      query += ` WHERE articles.topic = $1`;
+      query += groupAndOrder;
+      valueArr.push(topic);
+      return db.query(query, valueArr).then((data) => {
+        return data.rows;
+      });
+    });
+  } else {
+    query += groupAndOrder;
+    return db.query(query).then((data) => {
+      return data.rows;
+    });
+  }
 };
+
 exports.selectComments = (article_id) => {
-  let sql = format(`
-  SELECT
-  comment_id,
-  votes,
-  created_at,
-  author,
-  body,
-  article_id
-  FROM 
-    comments
-  WHERE
-  article_id = $1
-  ORDER BY
-  created_at DESC;
-    `);
-  return db.query(sql, [article_id]).then(({ rows }) => {
-    return rows;
+  let sql = `
+    SELECT *
+    FROM comments
+    WHERE article_id = $1
+    ORDER BY created_at DESC;
+  `;
+  return checkExists("articles", "article_id", article_id).then(() => {
+    return db.query(sql, [article_id]).then((data) => {
+      if (!data) {
+        return [];
+      }
+      return data.rows;
+    });
   });
 };
 
@@ -101,7 +104,6 @@ exports.deleteCommentById = (comment_id) => {
 
 exports.getAllUsers = () => {
   return db.query("SELECT * FROM users;").then((data) => {
-    console.log(data, "model");
     return data.rows;
   });
 };
