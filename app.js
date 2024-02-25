ca3753c625dca7855852d1800b2fbbb16804b8c0
Joe Mac <jhmccarney@gmail.com>
2024-02-25 14:33:03 +0000

    added homepage endpoint to test

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req,res) => {
+  res.send("HOME PAGE !!!")
+})
+
 app.get("/api/topics", getTopics);
 
 app.get("/api", getApi);
